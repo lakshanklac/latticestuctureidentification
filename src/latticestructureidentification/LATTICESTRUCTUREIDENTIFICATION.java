7bcee06320311223220137340adcb06ef5a6def6
K.L.A.C. LAKSHAN <K.L.A.C. LAKSHAN@KLACLAKSHAN>
2022-07-03 16:09:50 +0530

    Initial Commit.

---
+++ src/latticestructureidentification/LATTICESTRUCTUREIDENTIFICATION.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latticestructureidentification;
+
+/**
+ *
+ * @author K.L.A.C. LAKSHAN
+ */
+public class LATTICESTRUCTUREIDENTIFICATION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame lsi=new NewJFrame();
+        lsi.setVisible(true);
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/latticestructureidentification/jProgressBar1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latticestructureidentification;
+
+/**
+ *
+ * @author K.L.A.C. LAKSHAN
+ */
+class jProgressBar1 {
+    
+}
